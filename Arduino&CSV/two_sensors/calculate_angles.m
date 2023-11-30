36cc4034c4281fbdbd7873cdcc8dc94180ad95ed
FlorianChavernac <florian.chavernac@etud.univ-jfc.fr>
2023-11-30 18:36:08 -0500

    Test

--- Arduino&CSV/two_sensors/calculate_angles.m
+++ Arduino&CSV/two_sensors/calculate_angles.m
@@ -1,5 +1,7 @@
 function [angles] = calculate_angles(donnees)
     quat_angle= conj(donnees.sensor_2.quat) .* donnees.sensor_1.quat
     angles = (euler ( quat_angle,'XYZ','frame') ) ;
+    
+disp(quat_angle)
 
     
