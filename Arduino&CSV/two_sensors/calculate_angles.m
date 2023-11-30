6205c47537c16c4d2ff910b4c1dbbb691b6c1573
FlorianChavernac <florian.chavernac@etud.univ-jfc.fr>
2023-11-30 11:45:11 -0500

    Without calibration and catch

---
+++ Arduino&CSV/two_sensors/calculate_angles.m
@@ -0,0 +1,5 @@
+function [angles] = calculate_angles(donnees)
+    quat_angle= conj(donnees.sensor_2.quat) .* donnees.sensor_1.quat
+    angles = (euler ( quat_angle,'XYZ','frame') ) ;
+
+    
--- Arduino&CSV/two_sensors/main.m
+++ Arduino&CSV/two_sensors/main.m
@@ -1,1 +1,15 @@
 [donnees] = import_data_from_csv("data_from_arduino.csv");
+[angles] = calculate_angles(donnees)
+%[time]= calculate_catch_time(donnees)
+
+subplot(3,1,1)
+plot(rad2deg(angles(:,1)))
+title("Angle 1")
+subplot(3,1,2)
+plot(rad2deg(angles(:,2)))
+title("Angle 2")
+subplot(3,1,3)
+plot(rad2deg(angles(:,3)))
+title("Angle 3")
+
+
